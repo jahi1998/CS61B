a95393b2880b0b2b6f66e2c53122fe45e817f77f
jahi1998 <13691554018@163.com>
2021-02-20 09:18:27 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,22 +1,23 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Jiahui Li
  */
+import java.util.Scanner;
+
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n%2 == 0) {
+            return n/2;
         } else {
-            return n * 2;
+            return 3*n+1;
         }
     }
 
     public static void main(String[] args) {
-        int n = 5;
-        System.out.print(n + " ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入一个整数");
+        int n = input.nextInt();
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
